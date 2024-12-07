43242a408985ddec63ed8402ffed5a134a070c25
Win Nguyen <winnguyen1905.work@gmail.com>
2024-12-07 21:35:26 +0700

    search product request and nested record

--- src/main/java/com/winnguyen1905/product/core/builder/ElasticSearchQueryBuilder.java
+++ src/main/java/com/winnguyen1905/product/core/builder/ElasticSearchQueryBuilder.java
@@ -1,100 +1,100 @@
 package com.winnguyen1905.product.core.builder;
 
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.client.elc.NativeQuery;
 
+import com.winnguyen1905.product.core.model.request.SearchProductRequest;
+import com.winnguyen1905.product.util.CommonUtils;
+
+import co.elastic.clients.elasticsearch._types.SortOptions;
+import co.elastic.clients.elasticsearch._types.query_dsl.BoolQuery;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
+import co.elastic.clients.elasticsearch._types.query_dsl.TermsQueryField;
+import co.elastic.clients.json.JsonData;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class ElasticSearchQueryBuilder {
 
-  public static NativeQuery createSearchQuery(SearchRequest request) {
+  public static NativeQuery createSearchQuery(SearchProductRequest searchProductRequest) {
 
     BoolQuery.Builder boolQueryBuilder = new BoolQuery.Builder();
 
-    setMatchQuery(request, boolQueryBuilder);
-    setFilters(request, boolQueryBuilder);
+    setFilters(searchProductRequest, boolQueryBuilder);
+
+    setMatchQuery(searchProductRequest, boolQueryBuilder);
 
     Query finalQuery = Query.of(bool -> bool.bool(boolQueryBuilder.build()));
 
-    SortOptions sortOptions = getSortOptions(request);
+    SortOptions sortOptions = getSortOptions(searchProductRequest);
 
-    NativeQuery nativeQuery =
-        NativeQuery.builder().withQuery(finalQuery).withSort(List.of(sortOptions)).build();
+    NativeQuery nativeQuery = NativeQuery.builder().withQuery(finalQuery).withSort(List.of(sortOptions)).build();
 
-    setPagination(request, nativeQuery);
-
-    log.info("Elasticsearch Product search query ::: {}", nativeQuery.getQuery());
+    setPagination(searchProductRequest, nativeQuery);
 
     return nativeQuery;
   }
 
-  private static void setPagination(SearchRequest request, NativeQuery nativeQuery) {
-    if (Objects.nonNull(request.getPagination())) {
+  private static void setPagination(SearchProductRequest searchProductRequest, NativeQuery nativeQuery) {
+    if (Objects.nonNull(searchProductRequest.pagination())) {
       nativeQuery.setPageable(
-          Pageable.ofSize(request.getPagination().getPageSize())
-              .withPage(request.getPagination().getPageNo()));
+          Pageable.ofSize(searchProductRequest.pagination().getPageSize())
+              .withPage(searchProductRequest.pagination().getPageNo()));
     }
   }
 
-  private static SortOptions getSortOptions(SearchRequest request) {
+  private static SortOptions getSortOptions(SearchProductRequest searchProductRequest) {
     SortOptions.Builder sortOptionsBuilder = new SortOptions.Builder();
 
-    Utility.stream(request.getSorts())
+    CommonUtils.stream(searchProductRequest.getSorts())
         .forEach(
-            sort ->
-                sortOptionsBuilder.field(
-                    f ->
-                        f.field(sort.getField())
-                            .order(
-                                sort.getOrder().equals("asc") ? SortOrder.Asc : SortOrder.Desc)));
+            sort -> sortOptionsBuilder.field(
+                f -> f.field(sort.getField())
+                    .order(
+                        sort.getOrder().equals("asc") ? SortOrder.Asc : SortOrder.Desc)));
 
     return sortOptionsBuilder.build();
   }
 
-  private static void setMatchQuery(SearchRequest request, BoolQuery.Builder bq) {
-    if (StringUtils.hasLength(request.getSearchTerm())) {
+  private static void setMatchQuery(SearchProductRequest searchProductRequest, BoolQuery.Builder bq) {
+    if (StringUtils.hasLength(searchProductRequest.getSearchTerm())) {
       bq.must(
-          m ->
-              m.match(
-                  matchQuery ->
-                      matchQuery.query(q -> q.stringValue(request.getSearchTerm())).field("name")));
+          m -> m.match(
+              matchQuery -> matchQuery.query(q -> q.stringValue(searchProductRequest.getSearchTerm())).field("name")));
     }
   }
 
-  private static void setFilters(SearchRequest request, BoolQuery.Builder bq) {
-    Utility.stream(request.getFilters())
+  private static void setFilters(SearchProductRequest searchProductRequest, BoolQuery.Builder bq) {
+    CommonUtils.stream(searchProductRequest.getFilters())
         .forEach(
             filter -> {
-              TermsQueryField valueTerms =
-                  new TermsQueryField.Builder()
-                      .value(
-                          filter.getValues().stream()
-                              .map(String::toLowerCase)
-                              .map(FieldValue::of)
-                              .toList())
-                      .build();
+              TermsQueryField valueTerms = new TermsQueryField.Builder()
+                  .value(
+                      filter.getValues().stream()
+                          .map(String::toLowerCase)
+                          .map(FieldValue::of)
+                          .toList())
+                  .build();
 
-              TermsQuery termsQuery =
-                  TermsQuery.of(t -> t.field(filter.getField()).terms(valueTerms));
+              TermsQuery termsQuery = TermsQuery.of(t -> t.field(filter.getField()).terms(valueTerms));
               bq.filter(f -> f.terms(termsQuery));
             });
   }
 
   private static Query multipleTermsQuery(TermsQueryField terms) {
     return Query.of(
-        q ->
-            q.bool(
-                b ->
-                    b.filter(f -> f.terms(t -> t.field("features.Brand").terms(terms)))
-                        .filter(f -> f.terms(t -> t.field("category.name").terms(terms)))));
+        q -> q.bool(
+            b -> b.filter(f -> f.terms(t -> t.field("features.Brand").terms(terms)))
+                .filter(f -> f.terms(t -> t.field("category.name").terms(terms)))));
   }
 
   private static Query getSingleTerm(JsonData data) {
     return Query.of(
-        q ->
-            q.bool(
-                b ->
-                    b.filter(
-                        f -> f.term(t -> t.field("features.Brand").value(v -> v.anyValue(data))))));
+        q -> q.bool(
+            b -> b.filter(
+                f -> f.term(t -> t.field("features.Brand").value(v -> v.anyValue(data))))));
   }
 }
--- src/main/java/com/winnguyen1905/product/core/model/request/SearchProductRequest.java
+++ src/main/java/com/winnguyen1905/product/core/model/request/SearchProductRequest.java
@@ -1,7 +1,48 @@
 package com.winnguyen1905.product.core.model.request;
 
 import java.util.List;
+import java.util.Objects;
 
-import com.winnguyen1905.product.core.model.AbstractModel;
+import org.springframework.integration.annotation.Default;
 
-public record SearchProductRequest() {}
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record SearchProductRequest(
+    List<Sort> sorts,
+    String searchTerm,
+    List<Filter> filters,
+    Pagination pagination) {
+  public Pagination getPage() {
+    if (Objects.isNull(pagination)) {
+      return new SearchProductRequest.Pagination(0, 0);
+    }
+    return pagination;
+  }
+
+  public static record Pagination(
+      @Default int pageSize,
+      @Default int pageNo) {
+    public Pagination {
+      pageSize = pageSize == 0 ? Integer.MAX_VALUE : pageSize;
+      pageNo = pageNo == 0 ? 0 : pageNo;
+    }
+  }
+
+  @JsonInclude(JsonInclude.Include.NON_EMPTY)
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static record Filter(
+      String field,
+      List<String> values) {
+  }
+
+  @JsonInclude(JsonInclude.Include.NON_EMPTY)
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static record Sort(
+      String order,
+      String field) {
+  }
+
+}
