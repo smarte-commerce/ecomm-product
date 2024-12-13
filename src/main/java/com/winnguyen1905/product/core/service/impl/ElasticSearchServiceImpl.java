223884bb680c6feaada3f8be29b8128cd0a76a97
Win Nguyen <winnguyen1905.work@gmail.com>
2024-12-13 12:39:59 +0700

    elastic search service impl

---
+++ src/main/java/com/winnguyen1905/product/core/service/impl/ElasticSearchServiceImpl.java
@@ -0,0 +1,25 @@
+package com.winnguyen1905.product.core.service.impl;
+
+import com.winnguyen1905.product.core.model.Variation;
+import com.winnguyen1905.product.core.model.response.PagedResponse;
+import com.winnguyen1905.product.core.service.ElasticSearchService;
+import com.winnguyen1905.product.persistance.entity.EProduct;
+
+import co.elastic.clients.elasticsearch.core.SearchRequest;
+import reactor.core.publisher.Mono;
+
+public class ElasticSearchServiceImpl implements ElasticSearchService {
+
+  @Override
+  public Mono<Void> persistProduct(EProduct product) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'persistProduct'");
+  }
+
+  @Override
+  public Mono<PagedResponse<Variation>> searchProducts(SearchRequest request) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'searchProducts'");
+  }
+  
+}
